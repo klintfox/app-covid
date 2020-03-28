73cb4691ba65afa04c204de618d3d02bc59117ac
klint Roman <klint_f@hotmail.com>
2020-03-28 11:18:01 -0500

    agregando proyectos

---
+++ service-world-meter/src/main/java/com/servicio/world/meter/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.servicio.world.meter.utils;
+
+public class Constants {
+
+	public static final String URLTABLE = "https://www.worldometers.info/coronavirus/";
+	
+	public static final String FIRST_COUNTRY = "USA";
+}
